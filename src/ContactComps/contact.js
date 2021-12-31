f92c505389c9132af835dd4e3a0ab8bea49f9296
Nishanth Prajith <nishanth.prajith@gmail.com>
2021-12-31 16:43:21 +0530

    fixed contact page

--- src/ContactComps/contact.js
+++ src/ContactComps/contact.js
@@ -18,37 +18,54 @@ export default function About() {
         },
     }
 
-    const hoverTestAnimation = {
-        "initial": {
-            width: "50px"
-        },
-        "hover": {
-            width: "10.1rem"
-        }
-    }
-
-
     return (
         <div style = {{overflow: "none"}}>
             <Helmet>
                 <title>Contact Me Page | Nishanth Prajith</title>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
             </Helmet>
             <motion.div variants = {animation} initial = "initial" animate = "final" className = "contact">
                 <div style = {{display: "grid", placeItems: "center"}}>
                     <p className = "header">CONTACT ME</p>
                 </div>
                 <div style = {{paddingLeft: "5%"}}>
-                    <h1>Get in touch. </h1>
+                    <h1>
+                        Get in <br/>
+                        - touch. 
+                    </h1>
                     <p className = "message">
                         I am from India, living and studying in the United States of America. Feel
                         free to conact me to say hi or talk about an project. I will do my best to
                         reach out back to you.
                     </p>
-                    <motion.div variants = {hoverTestAnimation} whileHover = "hover" initial = "initial" className = "buttonFillOne">
-                        <a href = "mailto:nishanth.prajith@gmail.com">
-                            <p className = "button">Say Hello &#8594;</p>
-                        </a>
-                    </motion.div>
+                    <div className='social'>
+                        <div>
+                            <h3>Email</h3>
+                            <p>nishanth.prajith@gmail.com</p>
+                        </div>
+                        <div>
+                            <h3>Socials</h3>
+                            <p className='socialmedia'>
+                                <a href = "https://www.facebook.com/people/Nishanth-Prajith/100009547906410/" target = "_blank" rel="noreferrer noopener">
+                                    <i class="fa fa-facebook"></i>
+                                </a>
+                                <a href = "https://www.instagram.com/nishanthprajith/" target = "_blank" rel="noreferrer noopener">
+                                    <i class="fa fa-instagram"></i>
+                                </a> 
+                                <a href = "https://www.linkedin.com/in/nishanth-prajith" target = "_blank" rel="noreferrer noopener">
+                                    <i class="fa fa-linkedin"></i>
+                                </a> 
+                                <a href = "https://github.com/NishanthPrajith" target = "_blank" rel="noreferrer noopener">
+                                    <i class="fa fa-github"></i>
+                                </a>
+                            </p>
+                        </div>
+                        <div>
+                            <h3>From</h3>
+                            <p>NYC</p>
+                        </div>
+                    </div>
+                    
                 </div>
             </motion.div>
         </div>
--- src/footerComps/footer.js
+++ src/footerComps/footer.js
@@ -4,24 +4,11 @@ import './footer.css'
 export default function Footer() {
     return (
         <div>
-            <Helmet>
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-            </Helmet>
+            
             <footer>
-                <p>&copy; 2021 Nishanth Prajith</p> 
+                <p>Copyright &copy; 2021 </p> 
                 <p>
-                    <a href = "https://www.facebook.com/people/Nishanth-Prajith/100009547906410/" target = "_blank" rel="noreferrer noopener">
-                        <i class="fa fa-facebook"></i>
-                    </a>
-                    <a href = "https://www.instagram.com/nishanthprajith/" target = "_blank" rel="noreferrer noopener">
-                        <i class="fa fa-instagram"></i>
-                    </a> 
-                    <a href = "https://www.linkedin.com/in/nishanth-prajith" target = "_blank" rel="noreferrer noopener">
-                        <i class="fa fa-linkedin"></i>
-                    </a> 
-                    <a href = "https://github.com/NishanthPrajith" target = "_blank" rel="noreferrer noopener">
-                        <i class="fa fa-github"></i>
-                    </a>
+                    nishanth prajith
                 </p>
             </footer>
         </div>
